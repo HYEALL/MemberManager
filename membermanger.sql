d268b61d5ab9749d05dd8acd41c12786c108bbe9
성은 <kwakse0720@gmail.com>
2024-01-31 18:45:52 +0900

    detail page 추가

--- membermanger.sql
+++ membermanger.sql
@@ -3,13 +3,14 @@
 CREATE TABLE Member
 ( 
 ID INT PRIMARY KEY,
-Name VARCHAR(30) NOT NULL, 
+Name VARCHAR(30) NOT NULL,
+BirthDate DATE NOT NULL, 
 CreateDate DATE NOT NULL, 
 ExpirationDate DATE, 
 RemainCount INT, 
 Valid BOOLEAN
 );
 
-INSERT INTO Member(ID, Name, CreateDate, ExpirationDate, Valid) VALUES
-(1, '곽성은', '2023-12-31', '2024-03-01', True),
-(2, '곽은성', '2023-1-31', '2024-04-01', True);
+INSERT INTO Member(ID, Name, BirthDate, CreateDate, ExpirationDate, Valid) VALUES
+(1, '곽성은', '2001-01-01', '2023-12-31', '2024-03-01', True),
+(2, '곽은성', '2000-01-01', '2023-1-31', '2024-04-01', True);
