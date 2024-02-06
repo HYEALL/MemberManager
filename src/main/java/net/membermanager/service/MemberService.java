e89ca27c78685d589e1d227a924e2199d3cb7fb3
성은 <kwakse0720@gmail.com>
2024-02-06 23:44:07 +0900

    Service class 구현

--- src/main/java/net/membermanager/service/MemberService.java
+++ src/main/java/net/membermanager/service/MemberService.java
@@ -4,10 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.membermanager.dto.Member;
 import net.membermanager.mapper.MemberMapper;
